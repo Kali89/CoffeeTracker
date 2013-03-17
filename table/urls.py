df6128a6cd9fe0773542a7a57b24eee6d3e96af8
Kali89 <matthewsharpe3@gmail.com>
2013-03-17 16:35:05 +0000

    Added the first view

---
+++ table/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+
+from table import views
+
+urlpatterns = patterns('',
+        url(r'^$', views.index, name='index')
+                       )
--- table/views.py
+++ table/views.py
@@ -1,1 +1,4 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hi - you're at the table index.")
