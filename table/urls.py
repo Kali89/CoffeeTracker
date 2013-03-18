a90b86b6dc1be899320ab2e3918579c686dbd1f3
Kali89 <matthewsharpe3@gmail.com>
2013-03-18 23:16:28 +0000

    Added a proper view

--- table/urls.py
+++ table/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from table import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index')
+        url(r'^$', views.index, name='index'),
+        url(r'^hello/$', views.table, name='table'),
             )
--- table/views.py
+++ table/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 
+from table.models import Scores
+
 def index(request):
-    return HttpResponse("Hi - you're at the table index.")
+    latest_scores = Scores.objects.all()
+    output = ', '.join([p.buyer for p in latest_scores])
+    return HttpResponse(output)
+
+
+def table(request):
+    return HttpResponse("You're looking at the table page.")
