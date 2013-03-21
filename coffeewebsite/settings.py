5a659325b7e9ca7b53cb24a797c3979bdab820f5
Kali89 <matthewsharpe3@gmail.com>
2013-03-21 23:59:01 +0000

    Added an index url structure.

--- coffeewebsite/settings.py
+++ coffeewebsite/settings.py
@@ -111,9 +111,7 @@ ROOT_URLCONF = 'coffeewebsite.urls'
 WSGI_APPLICATION = 'coffeewebsite.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    PROJECT_PATH + '/templates/',
 )
 
 INSTALLED_APPS = (
